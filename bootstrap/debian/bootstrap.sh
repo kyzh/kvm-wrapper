950a7a14b276e853932bdcad7e4123ee033a0e80
Benjamin Cohen <bencoh@notk.org>
2011-12-25 14:05:08 +0100

    bootstrap/debian: Allow serial login

--- bootstrap/debian/bootstrap.sh
+++ bootstrap/debian/bootstrap.sh
@@ -241,6 +241,9 @@ EOF
 	sed -i -e "s@DEBIAN_MIRROR@$BOOTSTRAP_DEBIAN_MIRROR@" "$MNTDIR/etc/apt/sources.list"
 	sed -i -e "s/FLAVOR/$BOOTSTRAP_FLAVOR/" "$MNTDIR/etc/apt/sources.list"
 
+	# Allow login from serial console
+	echo "T0:23:respawn:/sbin/getty -L ttyS0 115200" >> "$MNTDIR/etc/inittab"
+
 	if [[ "$BOOTSTRAP_PARTITION_TYPE" == "msdos" ]]; then
 		desc_remove_setting "KVM_KERNEL"
 		desc_remove_setting "KVM_INITRD"
